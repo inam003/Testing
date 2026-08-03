c6572c4893020c44e1bef75588271089217365d0
inamaslam <inam.aslam@ngbs.co.uk>
2026-08-03 20:55:51 +0500

    Change 'Learn more' text to 'Learn more to become expert.'
    Updated text to encourage expertise in React.

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function App() {
             <li>
               <a href="https://react.dev/" target="_blank">
                 <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
+                Learn more to become expert.
               </a>
             </li>
           </ul>
