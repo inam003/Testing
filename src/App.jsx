54bb21fba10b7a9b3e7e61be8233c8e98b45b9c1
inamaslam <inam.aslam@ngbs.co.uk>
2026-08-08 09:33:23 +0500

    Change header text from LOCAL to GITHUB

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started with this line - LOCAL</h1>
+          <h1>Get started with this line - GITHUB</h1>
           <p>
             Edit <code>src/App.jsx</code> here and save to test <code>HMR</code>
           </p>
