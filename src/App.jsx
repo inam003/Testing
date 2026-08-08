34125b3e10a7188fe0b7adc267d644fdb7bba40f
Muhammad Inam Aslam <inam.aslam@ngbs.co.uk>
2026-08-08 20:02:11 +0500

    testing auto commit feature

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>WEB CHANGE THIS LINE AND CHECK</h1>
+          <h1>WEB CHANGE THIS LINE AND CHECK IF IT WORKS OR NOT</h1>
           <p>
             Edit <code>src/App.jsx</code> here and save to test <code>HMR</code>
           </p>
