47c180f036b746fecfdbe9887ee6eebdeffd0a68
Muhammad Inam Aslam <inam.aslam@ngbs.co.uk>
2026-08-09 09:05:27 +0500

    ll

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>WEB COMMIT THIS LINE AND CHECK IF IT WORKS OR NOT</h1>
+          <h1>LOCAL COMMIT THIS LINE AND CHECK IF IT WORKS OR NOT</h1>
           <p>
             Edit <code>src/App.jsx</code> here and save to test <code>HMR...</code>
           </p>
